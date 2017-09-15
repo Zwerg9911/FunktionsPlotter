718fc5dd0d5ba8a3d8407de1f50cca800792bec2
Zwerg9911 <31532065+Zwerg9911@users.noreply.github.com>
2017-09-15 08:57:10 +0200

    Add files via upload

--- FunktionsPlotter-master/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ FunktionsPlotter-master/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -41,15 +42,13 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(100, 100);
+            this.panel1.Location = new System.Drawing.Point(101, 76);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(499, 499);
+            this.panel1.Size = new System.Drawing.Size(500, 500);
             this.panel1.TabIndex = 1;
-            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
             // button2
             // 
@@ -59,13 +58,24 @@
             this.button2.TabIndex = 2;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(663, 41);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(749, 682);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.button1);
@@ -80,6 +90,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- FunktionsPlotter-master/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ FunktionsPlotter-master/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -16,71 +16,105 @@ namespace WindowsFormsApplication1
         public Form1()
         {
             InitializeComponent();
+           
+         
+           
+         
           
         }
-           Pen p1 = new Pen(Color.Black,1);
-           
-        private void button1_Click(object sender, EventArgs e)
+          Pen p1;   
+          Graphics Zeichenfläche;
+            int NiemalsJ = 10;
+            private void button1_Click(object sender, EventArgs e)
+      {
+         
+      }
+
+      private void panel1_Paint(object sender, PaintEventArgs e)
+      {
+
+
+
+
+
+
+
+
+
+
+
+
+          }
+          private void Optimiere_Fenster()
+          {
+              panel1.Width = this.ClientRectangle.Width - 50;
+              panel1.Height = this.ClientRectangle.Height - 100;
+
+
+              if (panel1.Width % 2 == 0)
+              {
+                  panel1.Width = panel1.Width - 1;
+              }
+              if (panel1.Height % 2 == 0)
+              {
+                  panel1.Height = panel1.Height - 1;
+              }
+
+
+
+
+          }
+
+
+      
+
+ 
+
+
+        private void button3_Click(object sender, EventArgs e)
         {
-            panel1.CreateGraphics();
+            for (int i = 0; i <= 1000000; i++)
+            {
+                Console.WriteLine(i);
+            }
             
         }
 
-        private void panel1_Paint(object sender, PaintEventArgs e)
+        private void button2_Click_1(object sender, EventArgs e)
         {
-            
-     
-            
+          Pen p1 = new Pen(Color.Black,1);
+          Zeichenfläche = panel1.CreateGraphics();
 
-
-
-
-                
-
-
-
-                
-            }
-            private void Optimiere_Fenster()
+          Point x_MO;
+          x_MO = new Point(panel1.Width / 2, 0);
+          Point x_MU;
+          x_MU = new Point(panel1.Width / 2, Height);
+          Point y_ML;
+          y_ML = new Point(0, panel1.Height / 2);
+          Point y_MR;
+          y_MR = new Point(Width, panel1.Height / 2);
+          Zeichenfläche.DrawLine(p1, x_MO, x_MU);
+          Zeichenfläche.DrawLine(p1, y_ML, y_MR);
+            for (int i = 1; i <= NiemalsJ; i++)
             {
-                panel1.Width = this.ClientRectangle.Width - 50;
-                panel1.Height = this.ClientRectangle.Height - 100;
+               int k = (panel1.Width/10);  
+                Point one;
+                one = new Point(i*k, panel1.Height / 2 + panel1.Height / 50);
+                Point two;
+                two = new Point(i*k, panel1.Height / 2 - panel1.Height / 50);
 
 
-                if (panel1.Width % 2 == 0)
-                {
-                    panel1.Width = panel1.Width - 1;
-                }
-                if (panel1.Height % 2 == 0)
-                {
-                    panel1.Height = panel1.Height - 1;
-                }
                 
+                int l = (panel1.Height / 10);
+                Point three;
+                three = new Point( panel1.Width / 2 + panel1.Width / 50, i * l);
+                Point four;
+                four = new Point(panel1.Width / 2 - panel1.Width / 50, i * l);
 
 
-
+                Zeichenfläche.DrawLine(p1, three, four);
+                Zeichenfläche.DrawLine(p1,one,two);
             }
-
-            private void panel1_Paint(object sender, PaintEventArgs e)
-            {
-
-            }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            
-            
-
-            Point x_MO;
-            x_MO = new Point(panel1.Width / 2, 0);
-            Point x_MU;
-            x_MU = new Point(panel1.Width / 2, Height);
-            Point y_ML;
-            y_ML = new Point(0, panel1.Height / 2);
-            Point y_MR;
-            y_MR = new Point(Width, panel1.Height / 2);
-            Zeichenflaeche.DrawLine(Zeichenstift, x_MO, x_MU);
-            Zeichenflaeche.DrawLine(Zeichenstift, y_ML, y_MR);
         }
     }
     }
